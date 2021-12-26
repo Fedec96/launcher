6ee1b7055605d48c0e6b4d73a183d552eb89d3ab
Fedec96 <fedec96@gmail.com>
2021-12-26 12:43:15 +0100

    Initial commit

---
+++ launcher/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    system("TITLE launcher");
+    system("launcher.bat");
+    return 0;
+}
---
+++ launcher/version.h
@@ -0,0 +1,21 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VER_FILEVERSION             1,0,0,0
+#define VER_FILEVERSION_STR         "1.0.0.0\0"
+
+#define VER_PRODUCTVERSION          1,0,0,0
+#define VER_PRODUCTVERSION_STR      "1.0.0.0\0"
+
+#define VER_COMPANYNAME_STR         "Firstname Lastname"
+#define VER_FILEDESCRIPTION_STR     "Launcher name"
+#define VER_INTERNALNAME_STR        "Launcher"
+#define VER_LEGALCOPYRIGHT_STR      "© 2020 Firstname Lastname" // Optional
+#define VER_LEGALTRADEMARKS1_STR    "All rights reserved" // Optional
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    "launcher.exe"
+#define VER_PRODUCTNAME_STR         "Launcher"
+
+#define VER_COMPANYDOMAIN_STR       "www.launcher.site" // Optional
+
+#endif // VERSION_H
